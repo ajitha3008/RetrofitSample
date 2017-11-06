bb360ac57cd327ea5a79059dab7b2d5910b174e0
s92 <s92@avaya.com>
2017-11-06 17:06:22 +0530

    Initial commit

---
+++ app/src/main/java/braingalore/retrofitsample/rest/ApiInterface.java
@@ -0,0 +1,20 @@
+package braingalore.retrofitsample.rest;
+
+import braingalore.retrofitsample.model.MovieResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/upcoming")
+    Call<MovieResponse> getUpcomingMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
+
